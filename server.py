01863ac93e25300f246d69f99b01944a6ae87d1e
jaked <jakedaly1997@hotmail.com>
2025-05-13 20:52:24 +0100

    update

--- server.py
+++ server.py
@@ -1,21 +1,16 @@
 from flask import Flask, jsonify, request, abort, send_from_directory
-from flask_cors import CORS, cross_origin
 import os
 app = Flask(__name__)
-cors = CORS(app)  # allow CORS for all domains on all routes.
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 # Import the eventDAO for database operations
 from eventDAO import eventDAO
 
 @app.route('/')
-@cross_origin()
 def index():
     return send_from_directory('.', 'eventviewer.html')
 
 #curl "http://127.0.0.1:5000/events"
 @app.route('/events')
-@cross_origin()
 def getAll():
     # Retrieve all events from the database
     results = eventDAO.getAll()
@@ -23,7 +18,6 @@ def getAll():
 
 #curl "http://127.0.0.1:5000/events/2"
 @app.route('/events/<int:id>')
-@cross_origin()
 def findById(id):
     # Retrieve a specific event by ID
     foundEvent = eventDAO.findByID(id)
@@ -31,7 +25,6 @@ def findById(id):
 
 #curl  -i -H "Content-Type:application/json" -X POST -d "{\"name\":\"Concert\",\"location\":\"Stadium\",\"date\":\"2025-05-15\",\"genre\":\"Music\",\"description\":\"A great concert!\",\"price\":50}" http://127.0.0.1:5000/events
 @app.route('/events', methods=['POST'])
-@cross_origin()
 def create():
     if not request.json:
         abort(400)
@@ -53,7 +46,6 @@ def create():
 
 #curl  -i -H "Content-Type:application/json" -X PUT -d "{\"name\":\"Updated Concert\",\"location\":\"Arena\",\"date\":\"2025-06-01\",\"genre\":\"Pop\",\"description\":\"Updated description\",\"price\":60}" http://127.0.0.1:5000/events/1
 @app.route('/events/<int:id>', methods=['PUT'])
-@cross_origin()
 def update(id):
     foundEvent = eventDAO.findByID(id)
     if not foundEvent:
@@ -86,7 +78,6 @@ def update(id):
     return jsonify(foundEvent)
 
 @app.route('/events/<int:id>', methods=['DELETE'])
-@cross_origin()
 def delete(id):
     eventDAO.delete(id)
     return jsonify({"done": True})
